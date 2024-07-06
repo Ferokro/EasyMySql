1686976d244091cce07cf0d41441d255661de831
Ferokro <ferdidundar05@gmail.com>
2024-07-06 17:22:10 +0300

    First Push

---
+++ Test0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Common;
+using static System.Math;
+using EasyMySql.Net;
+
+Console.WriteLine("Connecting...");
+var con = new DBConnection(@"Server=localhost,3306;Database=voicenet;User=root;");
+Console.WriteLine("Connected!");
+
+while (true)
+{
+    using var result = con.QueryAndExecuteParams("SELECT * FROM users WHERE userid=? OR username=?",
+        Console.ReadLine(),
+        Console.ReadLine()
+    );
+
+    result.dataReader.Cast<MySqlConnector.MySqlAttribute>();
+    result.ForEach(i => Console.WriteLine(result.GetValuesJson()));
+}
